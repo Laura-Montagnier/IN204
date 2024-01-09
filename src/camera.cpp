7486ba15f2082e7b400a5d07780d09f95efeb97d
nathan-champeil <nathan.champeil@ensta-paris.fr>
2024-01-09 18:29:27 +0100

    Moved class definitions in the header files

--- src/camera.cpp
+++ src/camera.cpp
@@ -1,38 +1,13 @@
 #include "camera.hpp"
 
+int main() {
 
-Vecteur a;
+    Vecteur a;
 
-class Camera{
+    Camera c;
 
-public:
+    c.image();
 
-    Camera(){};
-
-    int hauteur_ecran = 100;
-    int largeur_ecran = 200;
-
-    double fov = 150;
-
-
-    int pixel_size = 1;
-
-
-    void image(){
-
-        Vecteur position_camera;
-        Vecteur direction_camera(0, 0, 1);
-        Vecteur v_x(1, 0, 0);
-        Vecteur v_y(0, 1, 0);
-
-        for (int x = -hauteur_ecran/2; x < hauteur_ecran/2; x++){
-            for (int y = largeur_ecran/2; y < largeur_ecran/2; y++){
-                Vecteur point_ecran = position_camera + direction_camera * fov + x * v_x + y * v_y;
-
-                Rayon rayon(position_camera, point_ecran - position_camera);
-                // TODO: faire un truc avec le rayon 
-
-            }
-        }
-    }
-};
+    std::cout << "Hello\n";
+    return 0;
+}
--- src/volumes.cpp
+++ src/volumes.cpp
@@ -2,77 +2,12 @@
 
 // La classe vecteur servira aussi pour les points, puisqu'il s'agit de contenir le même type d'informations.
 
-class Vecteur {
-private:
-public:
-    double x;
-    double y;
-    double z;
-
-    //Constructeur
-    Vecteur() : x(0), y(0), z(0) {};
-    Vecteur(double x, double y, double z) : x(x), y(y), z(z) {};
-    Vecteur(Vecteur& autre) : x(autre.x), y(autre.y), z(autre.z) {};
-
-
-    //Pour afficher les infos du vecteur
-     void print() const {
-        std::cout << "3 coordonées (" << x << ", " << y << ", " << z << ")" << std::endl;
-    }
-
-    Vecteur operator+(Vecteur& autre){
-        return Vecteur(x + autre.x, y + autre.y, z + autre.z);
-    };
-
-    Vecteur operator*(double lambda){
-        return Vecteur(lambda*x, lambda*y, lambda*z);
-    };
-};
-
-
-class Sphere {
-private:
-public:
-    Vecteur centre;
-    double radius;
-    
-    // Constructeur
-    Sphere(Vecteur point, double r) : centre(point), radius(r) {}
-    Sphere(double x, double y, double z, double r) : centre(x, y, z), radius(r) {}
-    Sphere(Sphere& s) = default;
-
-    // Afficher les informations de la sphère
-    void print() const {
-        std::cout << "Sphère de centre (" << centre.x << ", " << centre.y << ", " << centre.z << ") et de rayon " << radius << std::endl;
-    }
-};
-
-class Rayon {
-private :
-public :
-    Vecteur origine;
-    Vecteur direction;
-
-    //Constructeur
-    Rayon(double x, double y, double z, double v_x, double v_y, double v_z) : origine(x, y, z), direction(v_x, v_y, v_z) {}
-
-    //Afficher les informations du rayon
-    void print() const {
-        std::cout << "Rayon d'origine ("<< origine.x <<", " << origine.y << ", " << origine.z << ") et de direction (" << direction.x << ", " << direction.y << ", " << direction.z << ")" << std::endl;
-     }
-
-
-};
-
-
 int main() {
     // Exemple d'utilisation de la classe Sphere
     Sphere maSphere(1.0, 2.0, 3.0, 4.0);
 
     // Afficher les informations sur la sphère
-    maSphere.print();
+    std::cout << maSphere << "\n";
 
     return 0;
 }
-
-
