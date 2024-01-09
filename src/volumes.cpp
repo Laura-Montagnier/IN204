0bedf34ae10e1e2c51fe2dec2e58e207aee160f2
nathan-champeil <nathan.champeil@ensta-paris.fr>
2024-01-09 17:13:54 +0100

    Added constructors for classes and operators for vectors

--- src/volumes.cpp
+++ src/volumes.cpp
@@ -1,44 +1,49 @@
-#include <iostream>
-#include <cmath>
+#include "volumes.hpp"
 
 // La classe vecteur servira aussi pour les points, puisqu'il s'agit de contenir le même type d'informations.
 
 class Vecteur {
 private:
-    double X;
-    double Y;
-    double Z;
-
 public:
-    //Constructeur
-    Vecteur(double x, double y, double z) : X(x), Y(y), Z(z) {}
+    double x;
+    double y;
+    double z;
 
-    //Accesseurs
-    double getCoordX() const {return X; }
-    double getCoordY() const {return Y; }
-    double getCoordZ() const {return Z; }
+    //Constructeur
+    Vecteur() : x(0), y(0), z(0) {};
+    Vecteur(double x, double y, double z) : x(x), y(y), z(z) {};
+    Vecteur(Vecteur& autre) : x(autre.x), y(autre.y), z(autre.z) {};
+
 
     //Pour afficher les infos du vecteur
      void print() const {
-        std::cout << "3 coordonées (" << coordX << ", " << coordY << ", " << coordZ << ")" << std::endl;
+        std::cout << "3 coordonées (" << x << ", " << y << ", " << z << ")" << std::endl;
     }
+
+    Vecteur operator+(Vecteur& autre){
+        return Vecteur(x + autre.x, y + autre.y, z + autre.z);
+    };
+
+    Vecteur operator*(double lambda){
+        return Vecteur(lambda*x, lambda*y, lambda*z);
+    };
 };
 
 
 class Sphere {
 private:
-    
-
 public:
     Vecteur centre;
     double radius;
     
     // Constructeur
-    Sphere(double x, double y, double z, double r) : centre(Vecteur(X, Y, Z)), radius(r) {}
+    Sphere(Vecteur point, double r) : centre(point), radius(r) {}
+    Sphere(double x, double y, double z, double r) : centre(x, y, z), radius(r) {}
+    Sphere(Sphere& s) = default;
 
     // Afficher les informations de la sphère
     void print() const {
-        std::cout << "Sphère de centre (" << X << ", " << Y << ", " << Z << ") et de rayon " << radius << std::endl;
+        std::cout << "Sphère de centre (" << centre.x << ", " << centre.y << ", " << centre.z << ") et de rayon " << radius << std::endl;
     }
 };
 
@@ -49,16 +54,15 @@ public :
     Vecteur direction;
 
     //Constructeur
-    Rayon(double x, double y, double z, double a, double b, double c) : origine(Vecteur(X, Y, Z)), direction(Vecteur(A, B, C)) {}
+    Rayon(double x, double y, double z, double v_x, double v_y, double v_z) : origine(x, y, z), direction(v_x, v_y, v_z) {}
 
     //Afficher les informations du rayon
     void print() const {
-        std::cout << "Rayon d'origine ("<< X <<", " << Y << ", " << Z << ") et de direction ("<< A <<", " << B << ", " << C << ") << std::endl;
+        std::cout << "Rayon d'origine ("<< origine.x <<", " << origine.y << ", " << origine.z << ") et de direction (" << direction.x << ", " << direction.y << ", " << direction.z << ")" << std::endl;
      }
 
 
 };
-
 
 
 int main() {
@@ -68,6 +72,7 @@ int main() {
     // Afficher les informations sur la sphère
     maSphere.print();
 
-   
     return 0;
 }
+
+
