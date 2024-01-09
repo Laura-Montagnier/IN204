4e95a18523fcad4b704d842ba9a57fc331f2ed0a
nathan-champeil <nathan.champeil@ensta-paris.fr>
2024-01-09 20:03:20 +0100

    Include cmath and iostream

--- src/defs.hpp
+++ src/defs.hpp
@@ -1,7 +1,9 @@
-#include<iostream>
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_ttf.h"
 #include "SDL2/SDL_mixer.h"
+#include "SDL2/SDL_ttf.h"
+
+#include <cmath>
+#include <iostream>
 
 #define WINDOW_WIDTH 100
 #define WINDOW_HEIGHT 100
