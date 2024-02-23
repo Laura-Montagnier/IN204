8728a4df85c6902bf7f021af250c662a9a5ddccd
nathan-champeil <nathan.champeil@ensta-paris.fr>
2024-02-23 20:28:44 +0100

    Corrigé bug couleurs trop sombres Visualisation normales Dégradé en fond

--- src/camera.cpp
+++ src/camera.cpp
@@ -4,18 +4,28 @@ Vecteur point{0, 0, 0};
 Vecteur normale{.1, 0, 1};
 Plan plan{point, normale};
 
-Sphere sphere(0, 40, 1, .05);
+Sphere sphere(0, 8, 1, 1);
+Sphere s2(-.4, 4, 1, .2);
 
-Union monde{&sphere, &plan}; // objet contenant toute la scène 3d
 
-Vecteur direction_lumiere{1, -0.5, 1}; // vecteur pointant vers le soleil (source ponctuelle à l'infini ?)
+// objet contenant toute la scène 3d, (utiliser shared pointers ?)
+Union monde{&sphere, &plan, &s2};
 
-Vecteur couleur_ciel{0, 180, 255}; // à adapter avec un type couleur ?
-Vecteur couleur_sol{120, 100, 80};
+Vecteur direction_lumiere{1, -1, 1}; // vecteur pointant vers le soleil (source ponctuelle à l'infini ?)
+// Vecteur direction_lumiere{0, -1, 0};
+
+Vecteur couleur_sol{0, 255, 0};
 
 void colore_pixel(Vecteur couleur, int i, int j) {
     SDL_SetRenderDrawColor(renderer, couleur.x, couleur.y, couleur.z, 255);
     SDL_RenderDrawPoint(renderer, j, i);
+}
+
+Vecteur couleur_ciel(int i, int max_i) {
+    // double k = (rayon.direction.y / rayon.direction.norme() + 1) / 2;
+    double k = (double)i / max_i;
+    return Vecteur(200, 220, 255) * k + Vecteur(0, 100, 255) * (1 - k);
+    // return Vecteur(0, 0, 0) * k + Vecteur(0, 60, 80) * (1 - k);
 }
 
 Camera::Camera() : position_camera(0, 0, 1),
@@ -26,11 +36,11 @@ Camera::Camera() : position_camera(0, 0, 1),
                    hauteur_ecran(WINDOW_HEIGHT),
                    taille_pixel(.01),
                    angle_vue(70 * M_PI / 180),
-                   distance_ecran(largeur_ecran / std::tan(angle_vue / 2)),
+                   distance_ecran(largeur_ecran*taille_pixel / std::tan(angle_vue / 2)),
                    centre_ecran(position_camera + distance_ecran * direction_camera){};
 
 void Camera::image() {
-    direction_lumiere *= 1 / (direction_lumiere * direction_lumiere);
+    direction_lumiere *= 1 / direction_lumiere.norme();
 
     std::cout << "camera.image()\n";
 
@@ -56,10 +66,12 @@ void Camera::image() {
                 couleur = couleur_sol;
                 // l'intensité varie avec cos(angle(normale,lumiere))
                 couleur *= std::max(0., intersection.normale * direction_lumiere);
-                // std::cout << intersection.normale << "\n";
+
+                // couleur = (intersection.normale + Vecteur(1, 1, 1)) *  .5 * 255; // Visualise normales
+
             } else {
                 // couleur = get_ambiant_light ...
-                couleur = couleur_ciel;
+                couleur = couleur_ciel(i, hauteur_ecran);
             }
             // Vecteur couleur = intersection.existe * couleur_sol + !intersection.existe * couleur_ciel;
 
@@ -71,7 +83,7 @@ void Camera::image() {
 // void Camera::image() {
 
 //     Vecteur vitesse = centre_ecran - position_camera;
-//     vitesse *= 1/sqrt(vitesse*vitesse);
+//     vitesse *= 1 / sqrt(vitesse * vitesse);
 //     Rayon rayon(position_camera, vitesse);
 
 //     std::cout << "ray " << rayon << "\n";
@@ -79,4 +91,9 @@ void Camera::image() {
 
 //     Intersection intersection;
 //     monde.calcul_intersection(rayon, intersection);
+
+//     Vecteur couleur = couleur_sol;
+//     // l'intensité varie avec cos(angle(normale,lumiere))
+//     couleur *= std::max(0., intersection.normale * direction_lumiere);
+//     std::cout << intersection.normale << couleur << "\n";
 // }
