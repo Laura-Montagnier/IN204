05b33dc0366e1ab0858ff412832f8343824063a6
nathan-champeil <nathan.champeil@ensta-paris.fr>
2024-02-28 10:12:08 +0100

    Rebonds itératifs de rayons

--- src/camera.cpp
+++ src/camera.cpp
@@ -3,11 +3,21 @@
 const bool affiche_normales = false;
 const bool correction_gamma = true;
 
-Materiau vert{0, 255, 0};
-Materiau vert_fonce{10, 65, 0};
+int nb_rayons = 1;
+int max_rebonds = 5;
+
+
+
+Materiau vert{0, .9, 0};
+Materiau vert_fonce{10. / 255, 65. / 255, 0};
+
 
 Vecteur point{0, 0, 0};
 Vecteur normale{.1, 0, 1};
+
+// Vecteur point{0, 5, 0};
+// Vecteur normale{0, -1, .1};
+
 Plan plan{point, normale, vert_fonce};
 
 Sphere sphere(0, 8, 1, 1, vert);
@@ -15,6 +25,7 @@ Sphere s2(-.4, 4, 1, .2);
 
 // objet contenant toute la scène 3d, (utiliser shared pointers ?)
 Union monde{&sphere, &plan, &s2};
+// Union monde{&plan};
 
 Vecteur direction_lumiere{1, -1, 1}; // vecteur pointant vers le soleil (source ponctuelle à l'infini ?)
 // Vecteur direction_lumiere{0, -1, 0};
@@ -22,20 +33,44 @@ Vecteur direction_lumiere{1, -1, 1}; // vecteur pointant vers le soleil (source 
 void colore_pixel(Vecteur couleur, int i, int j) {
     // correction gamma
     if (correction_gamma) {
-        couleur.x = sqrt(couleur.x / 255) * 255;
-        couleur.y = sqrt(couleur.y / 255) * 255;
-        couleur.z = sqrt(couleur.z / 255) * 255;
+        couleur.x = sqrt(couleur.x) * 255;
+        couleur.y = sqrt(couleur.y) * 255;
+        couleur.z = sqrt(couleur.z) * 255;
     }
 
-    SDL_SetRenderDrawColor(renderer, couleur.x, couleur.y, couleur.z, 255);
+    SDL_SetRenderDrawColor(renderer, (int)couleur.x, (int)couleur.y, (int)couleur.z, 255);
     SDL_RenderDrawPoint(renderer, j, i);
 }
 
 Vecteur couleur_ciel(int i, int max_i) {
     // double k = (rayon.direction.y / rayon.direction.norme() + 1) / 2;
     double k = (double)i / max_i;
-    return Vecteur(200, 220, 255) * k + Vecteur(0, 100, 255) * (1 - k);
-    // return Vecteur(0, 0, 0) * k + Vecteur(0, 60, 80) * (1 - k);
+    return Vecteur(200. / 255, 220. / 255, 255. / 255) * k + Vecteur(0. / 255, 100. / 255, 255. / 255) * (1 - k);
+    // return Vecteur(0./255, 0./255, 0./255) * k + Vecteur(0./255, 60./255, 80./255) * (1 - k);
+}
+
+Vecteur lance_rayon(Rayon &rayon) {
+    Vecteur couleur{1, 1, 1};
+    for (int i = 0; i < max_rebonds; i++) {
+        Intersection intersection;
+        monde.calcul_intersection(rayon, intersection);
+        if (intersection.existe) {
+            // std::cout << intersection << "\n";
+            couleur.x *= intersection.materiau.couleur.x;
+            couleur.y *= intersection.materiau.couleur.y;
+            couleur.z *= intersection.materiau.couleur.z;
+            rayon.origine = intersection.point;
+
+            couleur *= std::max(0., intersection.normale * direction_lumiere);
+
+            // rayon.direction = calcule_direction_rebond_aleatoire(intersection); // diffusion
+            rayon.direction = rayon.direction + 2 * (rayon.direction * intersection.normale) * intersection.normale; // reflexion
+        } else {
+            break;
+        }
+    }
+
+    return couleur;
 }
 
 Camera::Camera() : position_camera(0, 0, 1),
@@ -52,6 +87,8 @@ Camera::Camera() : position_camera(0, 0, 1),
 void Camera::image() {
     // crée la scène, à mettre dans une fonction
     direction_lumiere *= 1 / direction_lumiere.norme();
+    // plan.normale *= 1 / plan.normale.norme();
+
 
     std::cout << "camera.image()\n";
 
@@ -71,26 +108,44 @@ void Camera::image() {
             monde.calcul_intersection(rayon, intersection);
 
             Vecteur couleur;
-            if (intersection.existe) {
-                // couleur = rayon.couleur ...
+            // std::cout << couleur << "\n";
 
-                if (affiche_normales) {
-                    couleur = (intersection.normale + Vecteur(1, 1, 1)) * .5 * 255; // Visualise normales
-                } else {
-                    couleur = intersection.materiau.couleur;
-                    // l'intensité varie avec cos(angle(normale,lumiere))
-                    couleur *= std::max(0., intersection.normale * direction_lumiere);
-                }
+            for (int k = 0; k < nb_rayons; k++) {
+                Rayon rayon(position_camera, vitesse);
+                // rayon.origine += k*1e-4* Vecteur(1, 0, 0);
 
-            } else {
-                // couleur = get_ambiant_light ...
-                couleur = couleur_ciel(i, hauteur_ecran);
+                couleur += lance_rayon(rayon);
+                // std::cout << couleur << "\n";
             }
+            // couleur = lance_rayon(rayon);
+            // couleur = lance_rayon(rayon);
+            // std::cout << couleur << "\n";
+            couleur *= 1. / nb_rayons;
+            // std::cout << couleur << "\n";
+
+            // return
+            // if (intersection.existe) {
+            //     // couleur = rayon.couleur ...
+
+            //     if (affiche_normales) {
+            //         couleur = (intersection.normale + Vecteur(1, 1, 1)) * .5; // Visualise normales
+            //     } else {
+            //         couleur = intersection.materiau.couleur;
+            //         // l'intensité varie avec cos(angle(normale,lumiere))
+            //         // couleur *= std::max(0., intersection.normale * direction_lumiere);
+            //     }
+
+            // } else {
+            //     // couleur = get_ambiant_light ...
+            //     couleur = couleur_ciel(i, hauteur_ecran);
+            // }
             // Vecteur couleur = intersection.existe * couleur_sol + !intersection.existe * couleur_ciel;
 
             colore_pixel(couleur, i, j);
         }
     }
+
+    std ::cout << "fini \n";
 }
 
 // void Camera::image() {
@@ -103,10 +158,12 @@ void Camera::image() {
 //     std::cout << "sph " << sphere << "\n";
 
 //     Intersection intersection;
-//     monde.calcul_intersection(rayon, intersection);
+//     // monde.calcul_intersection(rayon, intersection);
 
-//     Vecteur couleur = couleur_sol;
+//     Vecteur couleur = lance_rayon(rayon);
+
+//     // Vecteur couleur = couleur_sol;
 //     // l'intensité varie avec cos(angle(normale,lumiere))
-//     couleur *= std::max(0., intersection.normale * direction_lumiere);
+//     // couleur *= std::max(0., intersection.normale * direction_lumiere);
 //     std::cout << intersection.normale << couleur << "\n";
 // }
